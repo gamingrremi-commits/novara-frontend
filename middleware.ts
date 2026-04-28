692936cda40e5cfdcf3579f2f8cdfe36df483a76
gamingrremi <gamingrremi@gmail.com>
2026-04-28 03:42:26 +0200

    fix middleware type

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request: { headers: request.headers } });
           cookiesToSet.forEach(({ name, value, options }) =>
